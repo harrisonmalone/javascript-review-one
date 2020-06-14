64fcdaf3d8f463c813bb98f8efa4b13e48f88fc0
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-06-15 09:43:02 +1000

    first question

--- javascript-review.js
+++ javascript-review.js
@@ -4,11 +4,25 @@
 
 // Create an object that has four properties. One property should be set so that the value is a number, another property set to a string, the third to an array, and the fourth to a function. This function should simply console.log the value of the first property.
 
+// myObject = {
+//   number : 2,
+//   string : 'This is a string',
+//   myArray : [],
+//   myFunc() {
+//     return 'this is my function return'
+//   }
+// }
+
+// console.log(myObject.number)
 // 2.
 
 // Write a constructor function named Car that takes one argument. It should use that argument to set the brand attribute of the car object. There should be a second attribute that stores the value for colour but this should be explicitly set to ‘red’. 
 
 // Note that a constructor function is the old school syntax that was used in javascript before the sugar class syntax came along in ES6. Don't use the sugar syntax for this question! 
+
+const car = function(brand) {
+  
+}
 
 // 3.
 
