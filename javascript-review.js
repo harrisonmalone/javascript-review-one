7bb7310b16c311b1b7321c0db1c6d3b25b6ffb1c
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-06-15 09:46:27 +1000

    second question

--- javascript-review.js
+++ javascript-review.js
@@ -9,13 +9,18 @@ const obj = {
   objString: "hello",
   objArray: [1, 3, 5],
   objFunc: () => {console.log("Hello")}
-}
+};
 
 // 2.
 
 // Write a constructor function named Car that takes one argument. It should use that argument to set the brand attribute of the car object. There should be a second attribute that stores the value for colour but this should be explicitly set to ‘red’. 
 
 // Note that a constructor function is the old school syntax that was used in javascript before the sugar class syntax came along in ES6. Don't use the sugar syntax for this question! 
+
+function Car(brand) {
+  this.brand = brand;
+  this.colour = "red";
+};
 
 // 3.
 
