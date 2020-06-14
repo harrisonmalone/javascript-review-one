c1db8eca658828db7fee831b043aac7a1677ffc7
Micah <Rus.Micah@gmail.com>
2020-06-15 09:20:18 +1000

    Question 1 completed

--- javascript-review.js
+++ javascript-review.js
@@ -4,6 +4,13 @@
 
 // Create an object that has four properties. One property should be set so that the value is a number, another property set to a string, the third to an array, and the fourth to a function. This function should simply console.log the value of the first property.
 
+
+let obj = {num:1, string:"String", array:[1,2,3], function: () => {
+  console.log(obj.num)
+}}
+
+console.log(obj);
+
 // 2.
 
 // Write a constructor function named Car that takes one argument. It should use that argument to set the brand attribute of the car object. There should be a second attribute that stores the value for colour but this should be explicitly set to ‘red’. 
