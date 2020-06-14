ead247997db550f6206c1f4ddccbce5ed38aa3e6
Micah <Rus.Micah@gmail.com>
2020-06-15 09:24:24 +1000

    Question 2 completed

--- javascript-review.js
+++ javascript-review.js
@@ -1,21 +1,20 @@
 // These questions are to test how you are progressing through the material, and to give you some feedback about where you might improve. Get through as many of these as you can. If you don’t get through all of them it’s ok - the test is designed to be difficult. If you can’t remember a bit of code, write the pseudo-code that represents how you would write your JS code.
-
 // 1.
-
 // Create an object that has four properties. One property should be set so that the value is a number, another property set to a string, the third to an array, and the fourth to a function. This function should simply console.log the value of the first property.
-
-
-let obj = {num:1, string:"String", array:[1,2,3], function: () => {
-  console.log(obj.num)
-}}
-
-console.log(obj);
-
+// let obj = {num:1, string:"String", array:[1,2,3], function: () => {
+//   console.log(obj.num)
+// }}
+// console.log(obj);
 // 2.
-
 // Write a constructor function named Car that takes one argument. It should use that argument to set the brand attribute of the car object. There should be a second attribute that stores the value for colour but this should be explicitly set to ‘red’. 
-
 // Note that a constructor function is the old school syntax that was used in javascript before the sugar class syntax came along in ES6. Don't use the sugar syntax for this question! 
+
+// class Car {
+//   constructor(brand) {
+//     this.colour = red
+//     this.brand = brand
+//   }
+// }
 
 // 3.
 
